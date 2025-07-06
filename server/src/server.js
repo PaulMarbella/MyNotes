95bf6cb82c7334c9a9250ac5d9efc6775a14e8cc
PaulMarbella <132251348+PaulMarbella@users.noreply.github.com>
2025-07-06 17:13:31 +0800

    prepared for delpoyment

--- server/src/server.js
+++ server/src/server.js
@@ -4,22 +4,35 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.POST_URL;
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
